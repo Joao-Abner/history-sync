c89086f9419ba8e89bb3a5f9b18613039f43c5be
Joao-Abner <joaoabnerpereira@gmail.com>
2024-11-11 20:03:44 -0300

    Added menu e footer component

---
+++ history-sync/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterOutlet,
+    FooterComponent,
+    MenuComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
