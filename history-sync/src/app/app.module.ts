b16114beb473b18bde37a1306b4fff1c6b52c34d
Joao-Abner <joaoabnerpereira@gmail.com>
2024-11-19 22:40:02 -0300

    "Updated cross-spawn version, modified app component HTML and TS, and changed footer and menu component HTML"

--- history-sync/src/app/app.module.ts
+++ history-sync/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +14,8 @@ import { MenuComponent } from './menu/menu.component';
   imports: [
     BrowserModule,
     RouterOutlet,
+    MenuComponent,
     FooterComponent,
-    MenuComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
