3e0b93df8083ab0478e5b317f4fae445972554f5
Joao-Abner <joaoabnerpereira@gmail.com>
2025-02-03 19:56:05 -0300

    feat: implement contact form with validation and local storage functionality

--- history-sync/src/app/contact/contact.component.ts
+++ history-sync/src/app/contact/contact.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  onSubmit(contactForm: NgForm) {
+    const name = contactForm.value.name;
+    const email = contactForm.value.email;
+
+    const contact = { name, email };
+    localStorage.setItem('contact', JSON.stringify(contact));
+    alert('Contato salvo com sucesso!');
+
+  }
 
 }
