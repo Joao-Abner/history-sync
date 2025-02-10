be3f6cc428034946e9ef7d767efb7ee1d89c8a2c
Joao-Abner <joaoabnerpereira@gmail.com>
2025-02-10 19:51:02 -0300

    Add app layout and styling:   header, and add global styles

--- history-sync/src/app/app.module.ts
+++ history-sync/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent,
     HeaderComponent
   ],
   imports: [
