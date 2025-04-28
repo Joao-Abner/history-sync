5bf9cd6cdf33b73cc0fb8677c1453a19619e1878
Joao-Abner <joaoabnerpereira@gmail.com>
2025-04-28 19:34:16 -0300

    `Added Bootstrap and Bootstrap Icons to Angular project, updated package.json and Angular configuration files, and modified HTML and CSS files to use Bootstrap components.`

--- history-sync/src/app/app.module.ts
+++ history-sync/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    FooterComponent
+    HeaderComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
