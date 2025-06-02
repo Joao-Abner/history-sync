9aa79521559dd5785a39d12467aabc8f9e5aa663
Joao-Abner <joaoabnerpereira@gmail.com>
2025-06-02 20:10:16 -0300

    Implement reactive form for NewEventPage; add validation for title, year, and description fields; enhance styles and structure for better user experience.

--- history-sync/src/app/app.module.ts
+++ history-sync/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { SearchEventsPageComponent } from './search-events-page/search-events-pa
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- history-sync/src/app/new-event-page/new-event-page.component.ts
+++ history-sync/src/app/new-event-page/new-event-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-event-page',
   templateUrl: './new-event-page.component.html',
-  styleUrl: './new-event-page.component.css'
+  styleUrls: ['./new-event-page.component.css']
 })
-export class NewEventPageComponent {
+export class NewEventPageComponent implements OnInit {
+  eventForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.eventForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      year: ['', [Validators.required, Validators.pattern(/^(-?\d{4})$/)]],
+      description: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.eventForm.valid) {
+      // Lógica para salvar o evento
+      console.log(this.eventForm.value);
+    }
+  }
 }
