d029937b218446ec0a05af7c7cd0a38055470e67
Joao-Abner <joaoabnerpereira@gmail.com>
2025-06-19 22:53:49 -0300

    Refactor MyEventService: update event interface and change method signatures for type safety

--- history-sync/src/app/services/my-events.service.ts
+++ history-sync/src/app/services/my-events.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Event {
+  id?: number;
+  title: string;
+  year: string;
+  description: string;
+}
+
 @Injectable({ providedIn: 'root' })
 export class MyEventService {
   private apiUrl = 'http://localhost:3000/my-events';
 
   constructor(private http: HttpClient) { }
 
-  getEvents(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>(this.apiUrl);
   }
 
-  addEvent(event: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, event);
+  addEvent(event: Event): Observable<Event> {
+    return this.http.post<Event>(this.apiUrl, event);
   }
 }
